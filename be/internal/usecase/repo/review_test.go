308fbad150ca222d2f5bb2dfb427d80c3ffb6509
SilverBeamx <molto.falso@gmail.com>
2023-05-04 22:43:25 +0200

    Implement Review tests

---
+++ be/internal/usecase/repo/review_test.go
@@ -0,0 +1,122 @@
+package repo_test
+
+import (
+	"context"
+
+	"github.com/just-hms/large-scale-multistructure-db/be/internal/entity"
+	"github.com/just-hms/large-scale-multistructure-db/be/internal/usecase/repo"
+)
+
+func (s *RepoSuite) TestReviewStore() {
+
+	userRepo := repo.NewUserRepo(s.db)
+	shopRepo := repo.NewBarberShopRepo(s.db)
+
+	user := &entity.User{Email: "giovanni"}
+	shop := &entity.BarberShop{Name: "brownies"}
+
+	err := userRepo.Store(context.Background(), user)
+	s.Require().NoError(err)
+
+	err = shopRepo.Store(context.Background(), shop)
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		name        string
+		input       *entity.Review
+		shopID      string
+		expectedErr bool
+	}{
+		{
+			name:        "Correctly inserted",
+			expectedErr: false,
+			shopID:      shop.ID,
+			input: &entity.Review{
+				Rating:  4,
+				Content: "test",
+				UserID:  user.ID,
+			},
+		},
+		{
+			name:        "The shop does not exists",
+			expectedErr: true,
+			shopID:      "test123",
+			input: &entity.Review{
+				Rating:  4,
+				Content: "test",
+				UserID:  user.ID,
+			},
+		},
+		{
+			name:        "The user does not exists",
+			expectedErr: true,
+			shopID:      shop.ID,
+			input: &entity.Review{
+				Rating:  4,
+				Content: "test",
+				UserID:  "test123",
+			},
+		},
+	}
+
+	reviewRepo := repo.NewReviewRepo(s.db)
+	for _, tc := range testCases {
+
+		s.Run(tc.name, func() {
+			err = reviewRepo.Store(context.Background(), tc.input, tc.shopID)
+			if tc.expectedErr {
+				s.Require().Error(err)
+				return
+			}
+
+			s.Require().NoError(err)
+			// check that the review was correctly created
+			// in the barbershop collection
+			shop, err = shopRepo.GetByID(context.Background(), shop.ID)
+			s.Require().NoError(err)
+			s.Require().Len(shop.Reviews, 1)
+		})
+	}
+
+}
+
+func (s *RepoSuite) TestReviewDelete() {
+
+	userRepo := repo.NewUserRepo(s.db)
+	shopRepo := repo.NewBarberShopRepo(s.db)
+	reviewRepo := repo.NewReviewRepo(s.db)
+
+	user := &entity.User{Email: "giovanni"}
+	shop := &entity.BarberShop{Name: "brownies"}
+
+	err := userRepo.Store(context.Background(), user)
+	s.Require().NoError(err)
+
+	err = shopRepo.Store(context.Background(), shop)
+	s.Require().NoError(err)
+
+	review := &entity.Review{
+		Rating:  4,
+		Content: "test",
+		UserID:  user.ID,
+	}
+
+	err = reviewRepo.Store(context.Background(), review, shop.ID)
+	s.Require().NoError(err)
+
+	// check that the review was correctly created
+	// in the barbershop collection
+	shop, err = shopRepo.GetByID(context.Background(), shop.ID)
+	s.Require().NoError(err)
+	s.Require().Len(shop.Reviews, 1)
+
+	err = reviewRepo.DeleteByID(context.Background(), shop.ID, review.ReviewID)
+	s.Require().NoError(err)
+
+	// check that the review was correctly deleted
+	// in the barbershop collection
+	shop, err = shopRepo.GetByID(context.Background(), shop.ID)
+	s.Require().NoError(err)
+	s.Require().Len(shop.Reviews, 0)
+
+}
