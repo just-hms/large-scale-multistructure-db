e0357991a763104f58b337ac6d94622746510c88
SilverBeamx <molto.falso@gmail.com>
2023-05-05 16:21:52 +0200

    Add Review Controller doc

--- be/internal/controller/review.go
+++ be/internal/controller/review.go
@@ -11,7 +11,6 @@ import (
 
 type ReviewRoutes struct {
 	reviewUseCase usecase.Review
-	userUseCase   usecase.User
 	tokenUseCase  usecase.Token
 }
 
@@ -27,6 +26,19 @@ type StoreReviewInput struct {
 	Content string `json:"content"`
 }
 
+// Store saves a Review in the given BarberShop
+//
+// @Summary Stores a Review in the given BarberShop
+// @Description Stores a Review in the given BarberShop
+// @Tags Review
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer token"
+// @Param shopid path string true "The ID of the barbershop"
+// @Param input body StoreReviewInput true "Rating and TextContent of a Review"
+// @Success 201 {object}  string ""
+// @Failure 400 {object}  string "Bad Request"
+// @Failure 401 {object}  string "Unauthorized"
 // @Router /barbershop/{shopid}/review [post]
 func (rr *ReviewRoutes) Store(ctx *gin.Context) {
 
@@ -61,6 +73,18 @@ func (rr *ReviewRoutes) Store(ctx *gin.Context) {
 
 }
 
+// ShowByShop retrieves all the Reviews in a given BarberShop
+//
+// @Summary ShowByShop retrieves all the Reviews in a given BarberShop
+// @Description ShowByShop retrieves all the Reviews in a given BarberShop
+// @Tags Review
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer token"
+// @Param shopid path string true "The ID of the barbershop"
+// @Success 200 {object}  map[string][]entity.Review
+// @Failure 400 {object} map[string]string
+// @Failure 401 {object}  string "Unauthorized"
 // @Router /barbershop/{shopid}/review [get]
 func (rr *ReviewRoutes) ShowByShop(ctx *gin.Context) {
 
@@ -76,6 +100,19 @@ func (rr *ReviewRoutes) ShowByShop(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"reviews": reviews})
 }
 
+// Delete removes a Review from a BarberShop
+//
+// @Summary Delete removes a Review from a BarberShop
+// @Description Delete removes a Review from a BarberShop
+// @Tags Review
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer token"
+// @Param shopid path string true "The ID of the barbershop"
+// @Param reviewid path string true "The ID of the Review"
+// @Success 202 {object}  string ""
+// @Failure 400 {object}  string "Bad Request"
+// @Failure 401 {object}  string "Unauthorized"
 // @Router /barbershop/{shopid}/review/{reviewid} [delete]
 func (rr *ReviewRoutes) Delete(ctx *gin.Context) {
 
@@ -97,6 +134,20 @@ type ReviewVoteInput struct {
 	Upvote bool `json:"upvote"`
 }
 
+// Vote saves an Upvote or Downvote for a Review in the given BarberShop
+//
+// @Summary Vote saves an Upvote or Downvote for a Review in the given BarberShop
+// @Description Vote saves an Upvote or Downvote for a Review in the given BarberShop
+// @Tags Review
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer token"
+// @Param shopid path string true "The ID of the barbershop"
+// @Param reviewid path string true "The ID of the Review"
+// @Param input body ReviewVoteInput true "Wheter a Vote is an Upvote("Upvote":true) or a Downvote("Upvote":false)"
+// @Success 201 {object}  string ""
+// @Failure 400 {object}  string "Bad Request"
+// @Failure 401 {object}  string "Unauthorized"
 // @Router /barbershop/{shopid}/review/{reviewid}/vote [post]
 func (rr *ReviewRoutes) Vote(ctx *gin.Context) {
 
@@ -132,6 +183,19 @@ func (rr *ReviewRoutes) Vote(ctx *gin.Context) {
 
 }
 
+// RemoveVote removes a Vote from a Review in a BarberShop
+//
+// @Summary RemoveVote removes a Vote from a Review in a BarberShop
+// @Description RemoveVote removes a Vote from a Review in a BarberShop
+// @Tags Review
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer token"
+// @Param shopid path string true "The ID of the barbershop"
+// @Param reviewid path string true "The ID of the Review"
+// @Success 202 {object}  string ""
+// @Failure 400 {object}  string "Bad Request"
+// @Failure 401 {object}  string "Unauthorized"
 // @Router /barbershop/{shopid}/review/{reviewid}/vote [delete]
 func (rr *ReviewRoutes) RemoveVote(ctx *gin.Context) {
 
