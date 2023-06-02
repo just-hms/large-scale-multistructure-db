3201d74b3f02d4487975cf85e3cb4a2d945a81f2
SilverBeamx <molto.falso@gmail.com>
2023-06-02 13:06:40 +0200

    Clean up Analytics tests

--- be/internal/usecase/repo/analytics_test.go
+++ be/internal/usecase/repo/analytics_test.go
@@ -9,32 +9,47 @@ import (
 	"github.com/just-hms/large-scale-multistructure-db/be/internal/usecase/repo"
 )
 
-func (s *RepoSuite) TestGetAppointmentViewReviewCountByShop() {
+var fixture map[byte]string
+
+const (
+	USER1_ID byte = iota
+	USER2_ID
+	ADMIN_ID
+	BARBER1_ID
+	BARBER2_ID
+
+	SHOP1_ID
+	SHOP2_ID
+)
+
+func (s *RepoSuite) SetupAnalyticsTestSuite() {
+
+	fixture = map[byte]string{}
 
 	user1 := &entity.User{Email: "giovanni"}
 	user2 := &entity.User{Email: "banana"}
 	shop1 := &entity.BarberShop{Name: "brownies"}
 	shop2 := &entity.BarberShop{Name: "choco"}
 
-	year, month, _ := time.Now().Date()
-	monthKey := fmt.Sprintf("%02d-%02d", year, month)
-
 	userRepo := repo.NewUserRepo(s.db)
 	shopRepo := repo.NewBarberShopRepo(s.db)
 	appointmentRepo := repo.NewAppointmentRepo(s.db)
 	viewRepo := repo.NewShopViewRepo(s.db)
 	reviewRepo := repo.NewReviewRepo(s.db)
-	analyticsRepo := repo.NewAnalyticsRepo(s.db)
 
 	err := userRepo.Store(context.Background(), user1)
 	s.Require().NoError(err)
+	fixture[USER1_ID] = user1.ID
 	err = userRepo.Store(context.Background(), user2)
 	s.Require().NoError(err)
+	fixture[USER2_ID] = user2.ID
 
 	err = shopRepo.Store(context.Background(), shop1)
 	s.Require().NoError(err)
+	fixture[SHOP1_ID] = shop1.ID
 	err = shopRepo.Store(context.Background(), shop2)
 	s.Require().NoError(err)
+	fixture[SHOP2_ID] = shop2.ID
 
 	app1 := &entity.Appointment{
 		StartDate:    time.Now().Add(1 * time.Hour),
@@ -92,26 +107,59 @@ func (s *RepoSuite) TestGetAppointmentViewReviewCountByShop() {
 	err = reviewRepo.Store(context.Background(), review2, shop2.ID)
 	s.Require().NoError(err)
 
-	analytics, err := analyticsRepo.GetAppointmentCountByShop(context.Background(), shop1.ID)
+}
+
+func (s *RepoSuite) TestGetAppointmentCountByShop() {
+
+	s.SetupAnalyticsTestSuite()
+
+	year, month, _ := time.Now().Date()
+	monthKey := fmt.Sprintf("%02d-%02d", year, month)
+
+	analyticsRepo := repo.NewAnalyticsRepo(s.db)
+
+	analytics, err := analyticsRepo.GetAppointmentCountByShop(context.Background(), fixture[SHOP1_ID])
 	s.Require().NoError(err)
 	s.Require().Equal(analytics[monthKey], 2)
-	analytics, err = analyticsRepo.GetAppointmentCountByShop(context.Background(), shop2.ID)
+	analytics, err = analyticsRepo.GetAppointmentCountByShop(context.Background(), fixture[SHOP2_ID])
 	s.Require().NoError(err)
 	s.Require().Equal(analytics[monthKey], 0)
 
-	analytics, err = analyticsRepo.GetViewCountByShop(context.Background(), shop1.ID)
+}
+
+func (s *RepoSuite) TestGetViewCountByShop() {
+
+	s.SetupAnalyticsTestSuite()
+
+	year, month, _ := time.Now().Date()
+	monthKey := fmt.Sprintf("%02d-%02d", year, month)
+
+	analyticsRepo := repo.NewAnalyticsRepo(s.db)
+
+	analytics, err := analyticsRepo.GetViewCountByShop(context.Background(), fixture[SHOP1_ID])
 	s.Require().NoError(err)
 	s.Require().Equal(analytics[monthKey], 4)
 
-	analytics, err = analyticsRepo.GetViewCountByShop(context.Background(), shop2.ID)
+	analytics, err = analyticsRepo.GetViewCountByShop(context.Background(), fixture[SHOP2_ID])
 	s.Require().NoError(err)
 	s.Require().Equal(analytics[monthKey], 1)
 
-	analytics, err = analyticsRepo.GetReviewCountByShop(context.Background(), shop1.ID)
+}
+
+func (s *RepoSuite) TestGetReviewCountByShop() {
+
+	s.SetupAnalyticsTestSuite()
+
+	year, month, _ := time.Now().Date()
+	monthKey := fmt.Sprintf("%02d-%02d", year, month)
+
+	analyticsRepo := repo.NewAnalyticsRepo(s.db)
+
+	analytics, err := analyticsRepo.GetReviewCountByShop(context.Background(), fixture[SHOP1_ID])
 	s.Require().NoError(err)
 	s.Require().Equal(analytics[monthKey], 1)
 
-	analytics, err = analyticsRepo.GetReviewCountByShop(context.Background(), shop2.ID)
+	analytics, err = analyticsRepo.GetReviewCountByShop(context.Background(), fixture[SHOP2_ID])
 	s.Require().NoError(err)
 	s.Require().Equal(analytics[monthKey], 2)
 }
