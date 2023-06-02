0ad715d4cc0adb385edb19b97309c4f912ce3bea
SilverBeamx <molto.falso@gmail.com>
2023-06-02 22:45:45 +0200

    Add Barber Analytics entity

---
+++ be/internal/entity/analyticsbarber.go
@@ -0,0 +1,11 @@
+package entity
+
+type BarberAnalytics struct {
+	AppointmentsByMonth         map[string]int
+	ViewsByMonth                map[string]int
+	ReviewsByMonth              map[string]int
+	AppointmentViewRatioByMonth map[string]float64
+	UpDownVoteCountByMonth      map[string]map[string]int
+	ReviewWeightedRating        float64
+	InactiveUsersList           []string
+}
