5803817cb0ca71bb69c46dc57480d014530e0fb3
SilverBeamx <molto.falso@gmail.com>
2023-06-24 00:42:58 +0200

    Setup docker backend files

--- docker/deploy/be/entrypoint.sh
+++ docker/deploy/be/entrypoint.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-echo "Waiting for db..."
-
-while ! nc -z db 27017; do
-    sleep 0.1
-done
-
-echo "db started"
-
-
-echo "Waiting for cache..."
-
-while ! nc -z cache 6379; do
-    sleep 0.1
-done
-
-echo "cache started"
-
-./cmd
+go run ./...
