1a3b2206e64d45bcc4382251f98be058db7fe3b1
just-hms <alessandroversari1@gmail.com>
2023-06-24 20:41:21 +0200

    init: index benchmarks

---
+++ be/internal/usecase/repo/index_test.go
@@ -0,0 +1,48 @@
+package repo_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/just-hms/large-scale-multistructure-db/be/config"
+	"github.com/just-hms/large-scale-multistructure-db/be/internal/usecase/repo"
+	"github.com/just-hms/large-scale-multistructure-db/be/pkg/mongo"
+)
+
+func BenchmarkIndexes(b *testing.B) {
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		fmt.Println("cannot find config")
+		return
+	}
+	mongo, err := mongo.New(cfg.Mongo.Host, cfg.Mongo.Port, "barbershop")
+	if err != nil {
+		fmt.Printf("mongo-error: %s", err.Error())
+		return
+	}
+	userRepo := repo.NewUserRepo(mongo)
+	barberShopRepo := repo.NewBarberShopRepo(mongo)
+
+	b.Run("user-GetByEmail-with-index", func(b *testing.B) {
+		userRepo.GetByEmail(context.Background(), "admin@admin.com")
+	})
+
+	b.Run("barbershop-Find-with-index", func(b *testing.B) {
+		barberShopRepo.Find(context.Background(), 0.200, 0.200, "", 10000)
+	})
+
+	// TODO remove the index
+
+	b.Run("user-GetByEmail", func(b *testing.B) {
+		userRepo.GetByEmail(context.Background(), "admin@admin.com")
+	})
+
+	b.Run("barbershop-Find-with-index", func(b *testing.B) {
+		barberShopRepo.Find(context.Background(), 0.200, 0.200, "", 10000)
+	})
+
+	// TODO add the index back
+
+}
